5f5e0c18b087082dec36b1acd5e4411bc797a16e
Girish Kumar Dewangan <123475394+GirishDewangan06@users.noreply.github.com>
2024-06-02 21:16:30 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+
+"use strict"//treated all js command as new version
+
+// alert(3+3)//we are using nodejs, not browser
+
+console.log(3
+    +3)//prefer readibility
+
+let name = "Girish"
+let age = 21
+let isYouBlind = "false"
+
+//1.number=> 2 to power 53 -1
+//2.bigInt
+//3.string => ""
+//4.boolean => true/false
+//5.null => standalone value
+//6.undefined
+
+//7.symbol => used for unique identification (majotly by figma tools)
+//8.object
+
+cnsole.log(typeof age);
+console.log(typeof null)// treated as the type of an object
+console.log(typeof undefined);//undefinedonsole.log(typeof "girish")
+co
